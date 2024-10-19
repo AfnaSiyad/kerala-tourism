a923b2fe71a3408f5e4b7f5052302ad958077c9f
Fthimathul Afna <afnafathima121@gmail.com>
2024-10-19 13:55:36 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -33,22 +33,4 @@ window.addEventListener('scroll', ()=>{
 
   });
 
-//kerala events part script start
 
-  let items = document.querySelectorAll('#eventsCarousel .carousel-item')
-
-		items.forEach((el) => {
-			const minPerSlide = 4
-			let next = el.nextElementSibling
-			for (var i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
-
-//keerala events part script end
